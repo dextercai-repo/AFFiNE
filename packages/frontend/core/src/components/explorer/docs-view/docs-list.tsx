fa3121e1a653557c86cc616910f62b49fcfd3c35
Cats Juice <wp1298554944@hotmail.com>
2025-05-16 17:06:16 +0800

    feat(mobile): new docs list for mobile

--- packages/frontend/core/src/components/explorer/docs-view/docs-list.tsx
+++ packages/frontend/core/src/components/explorer/docs-view/docs-list.tsx
@@ -56,15 +56,15 @@ const GroupHeader = memo(function GroupHeader({
   return header;
 });
 
-const calcCardHeightById = (id: string) => {
+const calcCardHeightById = (id: string, base = 250, scale = 10) => {
   if (!id) {
-    return 250;
+    return base;
   }
   const max = 5;
   const min = 1;
   const code = id.charCodeAt(0);
   const value = Math.floor((code % (max - min)) + min);
-  return 250 + value * 10;
+  return base + value * scale;
 };
 
 export const DocListItemComponent = memo(function DocListItemComponent({
@@ -80,9 +80,15 @@ export const DocListItemComponent = memo(function DocListItemComponent({
 export const DocsExplorer = ({
   className,
   disableMultiDelete,
+  masonryItemWidthMin,
+  heightBase,
+  heightScale,
 }: {
   className?: string;
   disableMultiDelete?: boolean;
+  masonryItemWidthMin?: number;
+  heightBase?: number;
+  heightScale?: number;
 }) => {
   const t = useI18n();
   const contextValue = useContext(DocExplorerContext);
@@ -112,7 +118,7 @@ export const DocsExplorer = ({
                 ? 42
                 : view === 'grid'
                   ? 280
-                  : calcCardHeightById(docId),
+                  : calcCardHeightById(docId, heightBase, heightScale),
             'data-view': view,
             className: styles.docItem,
           };
@@ -120,7 +126,7 @@ export const DocsExplorer = ({
       } satisfies MasonryGroup;
     });
     return items;
-  }, [groups, view]);
+  }, [groups, heightBase, heightScale, view]);
 
   const handleCloseFloatingToolbar = useCallback(() => {
     contextValue.selectMode$?.next(false);
@@ -192,7 +198,7 @@ export const DocsExplorer = ({
         groupsGap={12}
         groupHeaderGapWithItems={12}
         columns={view === 'list' ? 1 : undefined}
-        itemWidthMin={220}
+        itemWidthMin={masonryItemWidthMin ?? 220}
         preloadHeight={100}
         itemWidth={'stretch'}
         virtualScroll
